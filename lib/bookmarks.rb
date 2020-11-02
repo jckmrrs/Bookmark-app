b9f6ab773133a9c28e70fa88452f066eae2b72e0
Dan <daniel.bedford56@gmail.com>
2020-11-02 16:58:03 +0000

    Added basic db functionality

--- lib/bookmarks.rb
+++ lib/bookmarks.rb
@@ -1,12 +1,23 @@
+require 'pg'
+
 class Bookmarks
-  @@bookmarks = []
   
   def self.all
-    @@bookmarks
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
   def self.add(link)
-    @@bookmarks << link
+    connection = PG.connect(dbname: 'bookmark_manager')
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('#{link}');")
+    true
+  end
+
+  def self.delete(link)
+    connection = PG.connect(dbname: 'bookmark_manager')
+    connection.exec("DELETE FROM bookmarks WHERE url = '#{link}';")
+    true
   end
 
 end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,13 +1,29 @@
 require 'bookmarks'
 
 RSpec.describe Bookmarks do
-  describe '.all & add' do
-    it "User can add a bookmark and returns all of the user's bookmarks" do
-      Bookmarks.add("https://www.google.com")
-      Bookmarks.add("https://www.ford.com")
+
+  describe '.all' do
+    it "Returns all of the user's bookmarks" do
+      Bookmarks.add("http://www.google.com")
       bookmarks = Bookmarks.all
-      expect(bookmarks).to include("https://www.google.com")
-      expect(bookmarks).to include("https://www.ford.com")
+      expect(bookmarks).to include("http://www.google.com")
+      Bookmarks.delete("http://www.google.com")
     end
   end
+
+  describe '.add' do
+    it 'Adds a new bookmark to the database' do
+      expect(Bookmarks.add("http://www.google.com")).to eq true
+      Bookmarks.delete("http://www.google.com")
+    end
+  end
+
+  describe '.delete' do
+    it 'Adds a new bookmark to the database' do
+      Bookmarks.add("http://www.google.com")
+      expect(Bookmarks.delete("http://www.google.com")).to eq true
+    end
+  end
+
+  
 end
